bfe26b42f43373dd548436fd11c5269f93f7c061
Mohan Gupta <mohankrishnagupta@gmail.com>
2023-02-24 23:18:52 +0530

    Both image and video for streamlit app

--- app/streamlit.py
+++ app/streamlit.py
@@ -2,9 +2,12 @@ import streamlit as st
 
 import numpy as np
 from PIL import Image
+import cv2
+import moviepy.editor as moviepy
 
 import os
 import base64
+import tempfile
 
 from predict import get_results
 
@@ -25,8 +28,9 @@ def add_bg_from_local(image_file):
 
 contnt = '<p style="font-family:sans-serif; color:White;"> Self-driving cars also known as Autonomous driving '\
         "cars are key innovation which has revolutionized the automobile industry. Autonomous driving is a "\
-        "complex task which requires precise understanding of the environment. Semantic Segmentation provides "\
-        "the pixel wise information about the driving scenes.</p>"\
+        "complex task which requires precise understanding of the environment. In order to get precise pixel wise "\
+        "information of the driving scenes, semantic segmentation is used. To perform semantic segmentation we "\
+        "have used U-Net model with slight modifications.</p>"\
         '<p style="font-family:sans-serif; color:White;"> We have trained U-Net model on Indian Driving '\
         "Dataset(IDD) with Dice Score = 80.04%</p>"
 
@@ -39,22 +43,66 @@ if __name__ == '__main__':
     st.markdown(new_title, unsafe_allow_html=True)
     st.markdown(contnt, unsafe_allow_html=True)
 
+    choice = st.selectbox(label="Do you want to upload an image or video?",
+                          options=("Image", "Video"))
+
     uploaded_file = st.file_uploader("Choose a file")
-
-    col1, col2 = st.columns(2)
     
-    with col1:
+    if choice == "Image":
+        col1, col2 = st.columns(2)
+        with col1:
+            if uploaded_file is not None:
+                img = Image.open(uploaded_file)
+
+                img = img.resize((720, 480))
+                st.image(img)
+            
+                if st.button("Predict"):
+                    img_arr = np.array(img)
+                    pred = get_results(img_arr)
+
+                    masked_image = Image.fromarray(pred, 'RGB')
+                    masked_image = masked_image.resize((720, 480))
+                    with col2:
+                        st.image(masked_image)
+    
+    else:
         if uploaded_file is not None:
-            img = Image.open(uploaded_file)
+            output_frames = []
 
-            img = img.resize((720, 480))
-            st.image(img)
-        
-    if st.button("Predict"):
-        img_arr = np.array(img)
-        pred = get_results(img_arr)
+            tfile = tempfile.NamedTemporaryFile(delete=False) 
+            tfile.write(uploaded_file.read())
 
-        masked_image = Image.fromarray(pred, 'RGB')
-        masked_image = masked_image.resize((720, 480))
-        with col2:
-            st.image(masked_image)
+            vf = cv2.VideoCapture(tfile.name)
+
+            while vf.isOpened():
+                ret, frame = vf.read()  # if frame is read correctly ret is True
+                if not ret:
+                    print("Can't receive frame (stream end?). Exiting ...")
+                    break
+                
+                pred = get_results(frame)
+
+                masked_image = cv2.cvtColor(pred, cv2.COLOR_RGB2BGR)
+
+                output_frames.append(masked_image)
+
+            output_file = tempfile.NamedTemporaryFile(suffix='.avi', delete=False, dir=dir_path)
+
+            height, width, _ = output_frames[0].shape
+
+            fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+            video = cv2.VideoWriter(output_file.name, fourcc, 1, (width, height))
+
+            for frame in output_frames:
+                video.write(frame)
+
+            video.release()
+
+            clip = moviepy.VideoFileClip(output_file.name)
+            mp4_file = output_file.name[:-3]+"mp4"
+            print(mp4_file)
+
+            clip.write_videofile(mp4_file)
+
+            st.video(mp4_file)
